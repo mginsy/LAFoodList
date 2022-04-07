ffadb95575cc3472dd3c86125ef9c56ff85b9fac
Devin Mui <devinwmui@gmail.com>
2022-04-07 15:42:48 -0700

    add vanta

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,64 +1,60 @@
-import './splashScreen.css'
-import { useEffect, useState, useRef } from 'react'
-import BIRDS from 'vanta/dist/vanta.net.min.js'
-import { Container } from 'react-bootstrap'
+import "./splashScreen.css";
+import { useEffect, useState, useRef } from "react";
+import BIRDS from "vanta/dist/vanta.net.min.js";
+import { Container } from "react-bootstrap";
 
 function Home() {
-    const [vantaEffect, setVantaEffect] = useState(0)
-    const myRef = useRef(null)
-    useEffect(() => {
-        if (!vantaEffect) {
-            setVantaEffect(
-                BIRDS({
-                    el: myRef.current,
-                    color: '#89cff0',
-                    backgroundColor: 'black',
-                })
-            )
-        }
-        return () => {
-            if (vantaEffect) vantaEffect.destroy()
-        }
-    }, [vantaEffect])
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        BIRDS({
+          el: myRef.current,
+          color: "#89cff0",
+          backgroundColor: "black",
+        })
+      );
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
 
-    return (
-        <>
-        <div
-            ref={myRef}
-            style={{
-                height: '100%',
-                width: '100%',
-                position: 'absolute',
-                top: 0,
-                zIndex: -1,
-            }}
-        >
-            <Container style={{ margin: 'auto', paddingTop: 300 }}>
-                <div className="col-6">
-                    <h2 style={{ color: '#d3d3d3', fontSize: 30 }}>
-                        welcome to
-                    </h2>
-                    <div style={{ height: 10 }}></div>
-                    <h1 style={{ color: 'white', fontSize: 70 }}>apollo</h1>
-                    <div style={{ height: 10 }}></div>
-                    <h2
-                        style={{
-                            margin: 'auto',
-                            textAlign: 'left',
-                            color: '#d3d3d3',
-                            fontSize: 30,
-                        }}
-                    >
-                        a smart assistant that helps you stay connected with
-                        your doctor.
-                    </h2>
-                </div>
-            </Container>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div
+        ref={myRef}
+        style={{
+          height: "100%",
+          width: "100%",
+          position: "absolute",
+          top: 0,
+          zIndex: -1,
+        }}
+      >
+        <Container style={{ margin: "auto", paddingTop: 300 }}>
+          <div className="col-6">
+            <h2 style={{ color: "#d3d3d3", fontSize: 30 }}>welcome to</h2>
+            <div style={{ height: 10 }}></div>
+            <h1 style={{ color: "white", fontSize: 70 }}>apollo</h1>
+            <div style={{ height: 10 }}></div>
+            <h2
+              style={{
+                margin: "auto",
+                textAlign: "left",
+                color: "#d3d3d3",
+                fontSize: 30,
+              }}
+            >
+              a smart assistant that helps you stay connected with your doctor.
+            </h2>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
 }
-
 
 /*
 <div className="primary mb-5" style={{ height: '100%'}}> 
@@ -75,4 +71,4 @@ function Home() {
 
             */
 
-export default Home
+export default Home;
